102bb281b59d36b22c59869f55483f6ff08ab876
KMOON <93909632+AminKaammoun@users.noreply.github.com>
2023-12-09 15:36:07 +0100

    remove duplicated files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EsportsTour.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
