b4ddf82d4a0e3dc1d9d215dacb0f79b483c4ddb4
KMOON <93909632+AminKaammoun@users.noreply.github.com>
2023-12-13 18:30:34 +0100

    Fix migration

--- EsportsTour/Migrations/20231212225137_addImageToEquipe.cs
+++ EsportsTour/Migrations/20231212225137_addImageToEquipe.cs
@@ -10,21 +10,7 @@ namespace EsportsTour.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Joueurs_Users_UserId1",
-                table: "Joueurs");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Joueurs_UserId1",
-                table: "Joueurs");
-
-            migrationBuilder.DropColumn(
-                name: "UserId",
-                table: "Joueurs");
-
-            migrationBuilder.DropColumn(
-                name: "UserId1",
-                table: "Joueurs");
+           
 
             migrationBuilder.AddColumn<string>(
                 name: "image",
@@ -41,29 +27,7 @@ namespace EsportsTour.Migrations
                 name: "image",
                 table: "Equipes");
 
-            migrationBuilder.AddColumn<int>(
-                name: "UserId",
-                table: "Joueurs",
-                type: "int",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "UserId1",
-                table: "Joueurs",
-                type: "nvarchar(450)",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Joueurs_UserId1",
-                table: "Joueurs",
-                column: "UserId1");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Joueurs_Users_UserId1",
-                table: "Joueurs",
-                column: "UserId1",
-                principalTable: "Users",
-                principalColumn: "Id");
+           
         }
     }
 }
